34269ab6a97fc7160c622d735fa53c2552e2989f
Christopher Hall <hallgchris@gmail.com>
2020-12-03 22:49:37 +1100

    Add day 2 part 2 solution

--- day2/day2.py
+++ day2/day2.py
@@ -1,12 +1,17 @@
 import os
 import re
 
-def verify_password(min_count: int, max_count: int, policy_char: str, password: str):
+def verify_password_part_1(min_count: int, max_count: int, policy_char: str, password: str):
     counter = 0
     for char in password:
         if char == policy_char:
             counter += 1
     return min_count <= counter <= max_count
+
+def verify_password_part_2(first_index: int, second_index: int, policy_char: str, password: str):
+    first_result = first_index - 1 < len(password) and password[first_index - 1] == char
+    second_result = second_index - 1 < len(password) and password[second_index - 1] == char
+    return first_result ^ second_result
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(__file__)
@@ -14,7 +19,7 @@ if __name__ == "__main__":
 
     pattern = re.compile(r"(?P<min>\d+)-(?P<max>\d+) (?P<char>[a-z]): (?P<password>[a-z]*)")
 
-    valid = 0
+    valid_part_1, valid_part_2 = 0, 0
 
     with open(input_file) as file:
         for line in file:
@@ -23,7 +28,10 @@ if __name__ == "__main__":
             max_count = int(match.group("max"))
             char = match.group("char")
             password = match.group("password")
-            if verify_password(min_count, max_count, char, password):
-                valid += 1
+            if verify_password_part_1(min_count, max_count, char, password):
+                valid_part_1 += 1
+            if verify_password_part_2(min_count, max_count, char, password):
+                valid_part_2 += 1
 
-    print(f"{valid} valid passwords")
+    print(f"{valid_part_1} valid passwords (part 1 policy)")
+    print(f"{valid_part_2} valid passwords (part 2 policy)")
